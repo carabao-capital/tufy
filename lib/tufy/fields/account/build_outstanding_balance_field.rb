67125ed017dee99128d96b0e5bb0c36dda21fc39
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2017-07-18 12:27:59 +0800

    Zero if outstanding_balance is negative

--- lib/tufy/fields/account/build_outstanding_balance_field.rb
+++ lib/tufy/fields/account/build_outstanding_balance_field.rb
@@ -16,9 +16,12 @@ module Tufy
         def self.transform(ctx)
           raw_data = ctx.raw_data
 
+          outstanding_balance = raw_data[:outstanding_balance].to_i
+          outstanding_balance = outstanding_balance < 0 ? 0 : outstanding_balance
+
           BuildAccountSegment::Constants::OUTSTANDING_BALANCE_TAG +
-            FormatStrings::F2TS % raw_data[:outstanding_balance].to_i.to_s.size +
-            raw_data[:outstanding_balance].to_i.to_s
+            FormatStrings::F2TS % outstanding_balance.to_s.size +
+              outstanding_balance.to_s
         end
       end
     end
