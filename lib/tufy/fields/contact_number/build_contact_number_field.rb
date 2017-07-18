cff32a7e945e7861c5ce0a9f4605b36ea6f6fe9d
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2017-07-18 08:55:29 +0800

    Use ten zeros for blank contact numbers

--- lib/tufy/fields/contact_number/build_contact_number_field.rb
+++ lib/tufy/fields/contact_number/build_contact_number_field.rb
@@ -2,6 +2,10 @@ module Tufy
   module Fields
     module ContactNumber
       class BuildContactNumberField < BuildField
+        # When raw_data[:contact_number].blank?
+        # we use NULL_CONTACT_NUMBER instead
+        NULL_CONTACT_NUMBER = "0000000000"
+
         expects :raw_data
         promises :transformed_data
 
@@ -16,9 +20,15 @@ module Tufy
         def self.transform(ctx)
           raw_data = ctx.raw_data
 
+          contact_number = if raw_data[:contact_number].empty?
+                             NULL_CONTACT_NUMBER
+                           else
+                            raw_data[:contact_number]
+                           end
+
           BuildContactNumberSegment::Constants::CONTACT_NUMBER_TAG +
-            FormatStrings::F2TS % remove_special_characters(raw_data[:contact_number]).size +
-              remove_special_characters(raw_data[:contact_number])
+            FormatStrings::F2TS % remove_special_characters(contact_number).size +
+              remove_special_characters(contact_number)
         end
       end
     end
